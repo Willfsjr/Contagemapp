74d2be30ea6e51743b8ec5323deb602a0cb3e1b2
Willian Ferreira <willianfsjr@gmail.com>
2025-06-01 11:47:39 +0000

    Atualização e Criação da pagina de confronto do 1º Formulario.

---
+++ src/main/webapp/js/blockly/Conflito.blockly.js
@@ -0,0 +1,21 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Conflito = window.blockly.js.blockly.Conflito || {};
+
+/**
+ * @function atualizar
+ *
+ *
+ *
+ *
+ * @author Willian Ferreira
+ * @since 01/06/2025, 07:44:15
+ *
+ */
+window.blockly.js.blockly.Conflito.atualizarArgs = [];
+window.blockly.js.blockly.Conflito.atualizar = async function() {
+ var contNameConf, contObjConf;
+  //
+  this.cronapi.screen.refreshDatasource("conform1", 'true');
+}
