3b28613f0fe8056f5cd563efbbfcb1f360fc012f
Willian Ferreira <willianfsjr@gmail.com>
2025-06-01 20:42:57 +0000

    Atualizaçães e Criação da pagina de Conflito do 2º Formulario

--- src/main/webapp/js/blockly/Conflito1.blockly.js
+++ src/main/webapp/js/blockly/Conflito1.blockly.js
@@ -1,7 +1,7 @@
 window.blockly = window.blockly || {};
 window.blockly.js = window.blockly.js || {};
 window.blockly.js.blockly = window.blockly.js.blockly || {};
-window.blockly.js.blockly.Conflito = window.blockly.js.blockly.Conflito || {};
+window.blockly.js.blockly.Conflito1 = window.blockly.js.blockly.Conflito1 || {};
 
 /**
  * @function atualizar
@@ -10,11 +10,11 @@ window.blockly.js.blockly.Conflito = window.blockly.js.blockly.Conflito || {};
  *
  *
  * @author Willian Ferreira
- * @since 01/06/2025, 07:44:15
+ * @since 01/06/2025, 16:40:35
  *
  */
-window.blockly.js.blockly.Conflito.atualizarArgs = [];
-window.blockly.js.blockly.Conflito.atualizar = async function() {
+window.blockly.js.blockly.Conflito1.atualizarArgs = [];
+window.blockly.js.blockly.Conflito1.atualizar = async function() {
  var contNameConf, contObjConf;
   //
   this.cronapi.screen.refreshDatasource("conform1", 'true');
---
+++ src/main/webapp/js/blockly/Conflito2.blockly.js
@@ -0,0 +1,21 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Conflito2 = window.blockly.js.blockly.Conflito2 || {};
+
+/**
+ * @function atualizar
+ *
+ *
+ *
+ *
+ * @author Willian Ferreira
+ * @since 01/06/2025, 16:41:30
+ *
+ */
+window.blockly.js.blockly.Conflito2.atualizarArgs = [];
+window.blockly.js.blockly.Conflito2.atualizar = async function() {
+ var contNameConf2, contObjConf2;
+  //
+  this.cronapi.screen.refreshDatasource("conform2", 'true');
+}
